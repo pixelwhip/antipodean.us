d16aa97559bdec3b8e7a6ea00ea2e96f89ef2b20
John Ferris <john@pixel-whip.com>
2013-10-28 07:31:55 +1030

    Add CNAME file for gh-pages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -248,7 +248,8 @@ module.exports = function(grunt) {
               '*.{ico,png,txt}',
               '.htaccess',
               'images/{,*/}*.{webp,gif}',
-              'styles/fonts/{,*/}*.*'
+              'styles/fonts/{,*/}*.*',
+              '**/CNAME'
             ]
         }]
       },
